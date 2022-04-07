e579a8cb432d74d5c318c703d9e0f9187dab4dbb
Gabriel Montañola <gabriel@montanola.com>
2022-04-07 19:25:31 -0300

    chore: remove nvim stuff

--- dot_config/nvim/lua/plugins.lua
+++
@@ -1,9 +0,0 @@
-return require('packer').startup(function()
-  use {'wbthomason/packer.nvim', opt = true}
-
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use {'nvim-telescope/telescope.nvim',
-       requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}}
-  use {'neovim/nvim-lspconfig',
-        config = function() require 'plugins.nvim-lspconfig' end }
-end)
